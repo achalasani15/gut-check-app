acdb33aa6d03e360b4c43d136afcb4a1a59e0104
achalasani15 <chalasani.aashish@gmail.com>
2025-06-16 19:15:10 -0400

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,1 +1,11 @@
+import React from 'react';
+
+const Loader = ({ message }) => (
+    <div className="flex flex-col items-center justify-center h-screen bg-zinc-50">
+        <span className="text-5xl animate-bounce">🐾</span>
+        <p className="text-zinc-600 mt-4 text-lg">{message}</p>
+    </div>
+);
+
+export default Loader;
 
